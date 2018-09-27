d06d8fb3dfbbeb76cb8ddec6e69b11c5c85b21ea
Bram Van Grimberge <bvg@taxistop.be>
2018-09-27 10:54:51 +0200

    simplify button

--- views/event/form.php
+++ views/event/form.php
@@ -15,18 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $valid = true;
     $startHour = '00:00';
-    if(isset($_POST['eventHour']) && isset($_POST['eventMinute'])) {
+    if (isset($_POST['eventHour']) && isset($_POST['eventMinute'])) {
         $startHour = $_POST['eventHour'] . ':' . $_POST['eventMinute'];
     }
 
     $params = array(
-        'event_id' => !empty($_POST['eventId'])?$_POST['eventId']:null,
-        'long_desc' => !empty($_POST['eventDescription'])?$_POST['eventDescription']:"",
-        'category_id' => !empty($_POST['categoryId'])?$_POST['categoryId']:null,
-        'show_seperate' => !empty($_POST['eventShowSeperate'])?$_POST['eventShowSeperate']:false,
-        'start_date' => !empty($_POST['startDate'])?$_POST['startDate']:null,
+        'event_id' => !empty($_POST['eventId']) ? $_POST['eventId'] : null,
+        'long_desc' => !empty($_POST['eventDescription']) ? $_POST['eventDescription'] : "",
+        'category_id' => !empty($_POST['categoryId']) ? $_POST['categoryId'] : null,
+        'show_seperate' => !empty($_POST['eventShowSeperate']) ? $_POST['eventShowSeperate'] : false,
+        'start_date' => !empty($_POST['startDate']) ? $_POST['startDate'] : null,
         'start_hour' => $startHour,
-        'title' => !empty($_POST['title'])?$_POST['title']:null,
+        'title' => !empty($_POST['title']) ? $_POST['title'] : null,
     );
 
 
@@ -48,18 +48,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $valid = false;
     }
 
-    if($valid) {
+    if ($valid) {
         $db->saveOrUpdate($event);
     }
 }
 
-$mode = empty($event->getEventId())?'create':'edit';
+$mode = empty($event->getEventId()) ? 'create' : 'edit';
 
 ?>
 
 
 <div class="row">
-    <h3>Evenement <?php echo $mode == 'edit'?'wijzigen':'aanmaken' ?></h3>
+    <h3>Evenement <?php echo $mode == 'edit' ? 'wijzigen' : 'aanmaken' ?></h3>
 </div>
 <div class="row">
     <div class="offset-lg-3 col-lg-6">
@@ -68,17 +68,19 @@ $mode = empty($event->getEventId())?'create':'edit';
             <input type="hidden" name="eventId" value="<?php echo $event->getEventId() ?>">
             <div class="form-group">
                 <label for="title">Titel *</label>
-                <input type="text" class="form-control <?php echo in_array('title', $errors)?'is-invalid':'' ?>" id="title" name="title"
+                <input type="text" class="form-control <?php echo in_array('title', $errors) ? 'is-invalid' : '' ?>"
+                       id="title" name="title"
                        value="<?php echo $event->getTitle() ?>">
                 <div class="invalid-feedback">Verplicht veld</div>
             </div>
             <div class="form-group">
                 <label for="categoryId">Categorie *</label>
-                <select class="form-control <?php echo in_array('categoryId', $errors)?'is-invalid':'' ?>" id="categoryId" name="categoryId">
+                <select class="form-control <?php echo in_array('categoryId', $errors) ? 'is-invalid' : '' ?>"
+                        id="categoryId" name="categoryId">
                     <option></option>
                     <?php foreach ($categories as $category): ?>
                         <option
-                            value="<?php echo $category->getCategoryId() ?>"  <?php echo $event->getCategoryId() === $category->getCategoryId()?'selected':'' ?>><?php echo $category->getDescription(); ?></option>
+                            value="<?php echo $category->getCategoryId() ?>" <?php echo $event->getCategoryId() === $category->getCategoryId() ? 'selected' : '' ?>><?php echo $category->getDescription(); ?></option>
                     <?php endforeach; ?>
                 </select>
                 <div class="invalid-feedback">Verplicht veld</div>
@@ -86,7 +88,9 @@ $mode = empty($event->getEventId())?'create':'edit';
             <div class="form-group">
                 <label for="startDate">Startdatum *</label>
                 <input type="date" id="startDate" name="startDate" max="2100-12-31"
-                       min="<?php echo date("Y") ?>-01-01" class="form-control <?php echo in_array('startDate', $errors)?'is-invalid':'' ?>" value="<?php echo $event->getStartDate() ?>">
+                       min="<?php echo date("Y") ?>-01-01"
+                       class="form-control <?php echo in_array('startDate', $errors) ? 'is-invalid' : '' ?>"
+                       value="<?php echo $event->getStartDate() ?>">
                 <div class="invalid-feedback">Verplicht veld</div>
             </div>
             <div class="row">
@@ -95,7 +99,8 @@ $mode = empty($event->getEventId())?'create':'edit';
                         <label for="eventHour">Startuur</label>
                         <select class="form-control" id="" name="eventHour">
                             <?php foreach ($hours as $hour): ?>
-                                <option value="<?php echo $hour ?>" <?php echo $event->getHour() === $hour?'selected':'' ?> ><?php echo $hour ?></option>
+                                <option
+                                    value="<?php echo $hour ?>" <?php echo $event->getHour() === $hour ? 'selected' : '' ?> ><?php echo $hour ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
@@ -105,7 +110,8 @@ $mode = empty($event->getEventId())?'create':'edit';
                         <label for="eventMinute">Minuten</label>
                         <select class="form-control" id="eventMinute" name="eventMinute">
                             <?php foreach ($minutes as $minute): ?>
-                                <option value="<?php echo $minute ?>" <?php echo $event->getMinute() === $minute?'selected':'' ?> ><?php echo $minute ?></option>
+                                <option
+                                    value="<?php echo $minute ?>" <?php echo $event->getMinute() === $minute ? 'selected' : '' ?> ><?php echo $minute ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
@@ -123,12 +129,8 @@ $mode = empty($event->getEventId())?'create':'edit';
                     Toon in aparte slide
                 </label>
             </div>
-            <?php if ($mode == 'edit') : ?>
-                <button type="submit" class="btn btn-primary btn-lg btn-block mt-3">Opslaan</button>
-            <?php endif ?>
-            <?php if ($mode == 'create') : ?>
-                <button type="submit" class="btn btn-primary btn-lg btn-block mt-3">Toevoegen</button>
-            <?php endif ?>
+            <button type="submit"
+                    class="btn btn-primary btn-lg btn-block mt-3"><?php echo $mode == 'edit' ? 'Opslaan' : 'Toevoegen' ?></button>
         </form>
     </div>
 </div>
